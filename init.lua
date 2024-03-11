f827a86c6bbd3f9f432f7b3904aa1d24e7502733
Olivia Taliesin <olivia@taliesin.dev>
2024-03-11 21:21:11 +0100

    Fix Paths

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+local system = vim.loop.os_uname().sysname
+
+if system:find('Windows') then
+	if vim.g.msys == nil then
+		vim.g.msys = 'C:\\MSYS'
+		vim.env.PATH = vim.env.PATH .. vim.g.msys .. '\\usr\\local\\bin;' .. vim.g.msys .. '\\usr\\bin;' .. vim.g.msys .. '\\bin;' .. vim.g.msys .. '\\opt\\bin'
+	end
+end
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
@@ -14,8 +22,6 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-local system = vim.loop.os_uname().sysname
-
 require('lazy').setup('plugins', {
 	change_detection = {
 		enabled = false
@@ -26,10 +32,17 @@ if vim.g.dotvim == nil then
 	error('No dotvim defined')
 end
 
+
 if system:find('Windows') then
-	vim.cmd('source ' .. vim.g.dotvim .. '.winrc')
+	if vim.g.winrc == nil then
+		vim.g.winrc = vim.g.dotvim .. '.winrc'
+	end
+	vim.cmd('source ' .. vim.g.winrc)
 else
-	vim.cmd('source ' .. vim.g.dotvim .. '.vimrc')
+	if vim.g.vimrc == nil then
+		vim.g.vimrc = vim.g.dotvim .. '.vimrc'
+	end
+	vim.cmd('source ' .. vim.g.vimrc)
 end
 
 require('lsp')
